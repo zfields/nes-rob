1f6ff2fb0bce4a0bab2e475553458b1a6190af29
Zachary J. Fields <zachary_fields@yahoo.com>
2021-02-28 09:47:35 -0600

    feat: test coverage

--- test/run_all_tests.sh
+++ test/run_all_tests.sh
@@ -1,1 +1,25 @@
-g++ NesRob.test.cpp signal_generator.test.cpp ../src/signal_generator.cpp ../src/signal_generator_error.cpp led_ntsc_driver.test.cpp ../src/led_ntsc_driver.cpp w_wave_driver.test.cpp ../src/w_wave_driver.cpp ../src/pulse_driver_error.cpp ../src/hal_error.cpp -Wall -Wpedantic -I. -I ../src/ -I Catch2/single_include/ -I FakeIt/single_header/catch/ -DRUN_ALL_TESTS && ./a.out
+#!/bin/sh
+
+g++ -fprofile-arcs -ftest-coverage -Wall -Wpedantic -O0 -g \
+  NesRob.test.cpp \
+  signal_generator.test.cpp \
+  ../src/signal_generator.cpp \
+  ../src/signal_generator_error.cpp \
+  led_ntsc_driver.test.cpp \
+  ../src/led_ntsc_driver.cpp \
+  w_wave_driver.test.cpp \
+  ../src/w_wave_driver.cpp \
+  ../src/pulse_driver_error.cpp \
+  ../src/hal_error.cpp \
+-I. \
+-I ../src/ \
+-I Catch2/single_include/ \
+-I FakeIt/single_header/catch/ \
+-DRUN_ALL_TESTS \
+-o nes_rob_tests \
+&& ./nes_rob_tests \
+&& gcovr --print-summary --sort-percentage --exclude-throw-branches --delete \
+  --object-directory . \
+  --root ../src \
+  --exclude .*_error.* \
+&& rm *.gcno
